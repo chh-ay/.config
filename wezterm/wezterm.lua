7e7eaebe1c7068608c1f313d3f2b15f6956714a3
vinchhay sea <80159858+chh-ay@users.noreply.github.com>
2023-09-23 00:00:03 +0700

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,20 @@
 local wezterm = require("wezterm")
-local default_prog
+local config = {}
 
--- if wezterm.target_triple == 'x86_64-pc-windows-mscv' then
-
-if
-	wezterm.target_triple == "x86_64-unknown-linux-gnu"
-	or wezterm.target_triple == "x86_64-apple-darwin"
-	or wezterm.target_triple == "aarch64-apple-darwin"
-then
-	default_prog = { "/usr/bin/fish", "-l" }
+if wezterm.target_triple == 'x86_64-pc-windows-mscv' then
+	config.default_domain = "WSL:Ubuntu-22.04"
+elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+	config.default_prog = { "/usr/bin/fish", "-l" }
+	config.default_domain = "local"
+elseif wezterm.target_triple == "x86_64-apple-darwin" or wezterm.target_triple == "aarch64-apple-darwin" then
+	config.default_prog = { "/usr/local/bin/fish", "-l" }
+	config.default_domain = "local"
 end
 
 return {
 	color_scheme = "Catppuccin Mocha",
-	default_prog = default_prog,
+	default_prog = config.default_prog,
+	default_domain = config.default_domain,
 
 	font_size = 12.0,
 	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium", stretch = "Normal", style = "Normal" }),
