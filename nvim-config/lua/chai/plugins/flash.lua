030e7f636416f416881af0e1f6f92da1abc66cf5
chhay <vinchhaysea30@gmail.com>
2023-09-30 20:42:52 +0700

    Added some plugins, doesn't care about vscode for now

---
+++ nvim-config/lua/chai/plugins/flash.lua
@@ -0,0 +1,9 @@
+local maps = require "chai.mappings"
+
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  vscode = true,
+  opts = {},
+  keys = maps.flash,
+}
---
+++ nvim-config/lua/chai/plugins/gitsign.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "󰍵" },
+        topdelete = { text = "" },
+        changedelete = { text = "~" },
+        untracked = { text = "▎" },
+      },
+      current_line_blame = true,
+      on_attach = function(buffer)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+        end
+
+        map("n", "<leader>gb", gs.toggle_current_line_blame, "[G]it [B]lame Toggle")
+      end,
+    },
+  },
+}
