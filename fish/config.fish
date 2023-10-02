e1a2cf62b22daf9d5f51b6a786b60eafa23cf3a7
vinchhay <vinchhay.sea@sisgroup.sg>
2023-10-02 14:00:00 +0700

    Changed from exa to eza(eza is community maintained)

--- fish/config.fish
+++ fish/config.fish
@@ -64,11 +64,11 @@ end
 
 ## Useful aliases
 # Replace ls with exa
-alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
-alias la='exa -a --color=always --group-directories-first --icons' # all files and dirs
-alias ll='exa -l --color=always --group-directories-first --icons' # long format
-alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
-alias l.='exa -ald --color=always --group-directories-first --icons .*' # show only dotfiles
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons' # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons' # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.='eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
 alias ip='ip -color'
 
 # Replace some more things with better alternatives
