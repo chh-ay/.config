df02b9a57b9fb63bb8e6713f838237a4138b19a5
vinchhay <vinchhay.sea@sisgroup.sg>
2023-10-03 15:38:30 +0700

    None-ls completed (null-ls maintained by community since null-ls is now archived)
    might have some bugs havent test yet

---
+++ nvim-config/lua/chai/plugins/none-ls.lua
@@ -0,0 +1,37 @@
+return {
+  "nvimtools/none-ls.nvim",
+  event = "BufReadPre",
+  opts = function()
+    local none_ls = require "none-ls"
+    local formatting = none_ls.builtins.formatting
+    local diagnostics = none_ls.builtins.diagnostics
+    local code_actions = none_ls.builtins.code_actions
+    local completion = none_ls.builtins.completion
+    return {
+      debug = true,
+      sources = {
+        -- Formatting
+        formatting.shfmt,
+        formatting.prettierd,
+        formatting.clang_format,
+        formatting.taplo, -- toml file
+        formatting.black, -- for python xd?? my favorite formatter "black"
+        formatting.fish_indent,
+
+        -- Diagnostics
+        diagnostics.fish,
+        diagnostics.trail_space,
+        diagnostics.cspell.with {
+          diagnostics_postprocess = function(diagnostic)
+            diagnostic.severity = vim.diagnostic.severity["WARN"]
+          end,
+        },
+        diagnostics.editorconfig_checker,
+
+        -- Code Actions
+        code_actions.gitsigns,
+        code_actions.cspell,
+      },
+    }
+  end,
+}
