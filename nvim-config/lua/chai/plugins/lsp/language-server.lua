46c73bd19ef6c9759a6f1532a334b2f8e09117b0
vinchhay <vinchhay.sea@sisgroup.sg>
2023-10-03 16:12:10 +0700

    Testing (In progress)

--- nvim-config/lua/chai/plugins/lsp/language-server.lua
+++ nvim-config/lua/chai/plugins/lsp/language-server.lua
@@ -1,51 +1,107 @@
 return {
-  -- LSP Configuration & Plugins
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    -- Automatically install LSPs to stdpath for neovim
-    { "williamboman/mason.nvim", config = true },
-    "williamboman/mason-lspconfig.nvim",
+  {
+    -- LSP Configuration & Plugins
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      { "williamboman/mason.nvim", config = true },
+      "williamboman/mason-lspconfig.nvim",
 
-    -- Useful status updates for LSP
-    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    { "j-hui/fidget.nvim", tag = "legacy", opts = {} },
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { "j-hui/fidget.nvim", tag = "legacy", opts = {} },
 
-    -- Additional lua configuration, makes nvim stuff amazing!
-    "folke/neodev.nvim",
-  },
+      -- Additional lua configuration, makes nvim stuff amazing!
+      "folke/neodev.nvim",
+    },
 
-  config = function()
-    local servers = {
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- tsserver = {},
-      -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+    config = function()
+      local servers = {
+        clangd = {},
+        gopls = {},
+        pyright = {},
+        rust_analyzer = {},
+        tsserver = {},
+        html = { filetypes = { "html", "twig", "hbs" } },
 
-      lua_ls = {
-        Lua = {
-          workspace = { checkThirdParty = false },
-          telemetry = { enable = false },
+        lua_ls = {
+          Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+          },
         },
+      }
+      -- Ensure the servers above are installed
+      local mason_lspconfig = require "mason-lspconfig"
+
+      mason_lspconfig.setup {
+        ensure_installed = vim.tbl_keys(servers),
+      }
+
+      mason_lspconfig.setup_handlers {
+        function(server_name)
+          require("lspconfig")[server_name].setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = servers[server_name],
+            filetypes = (servers[server_name] or {}).filetypes,
+          }
+        end,
+      }
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    build = ":MasonUpdate",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        "bash-language-server",
+        "black",
+        "clang-format",
+        "clangd",
+        "codelldb",
+        "cspell",
+        "css-lsp",
+        "eslint-lsp",
+        "graphql-language-service-cli",
+        "html-lsp",
+        "json-lsp",
+        "lua-language-server",
+        "markdownlint",
+        "prettier",
+        "pyright",
+        "shfmt",
+        "tailwindcss-language-server",
+        "taplo",
+        "typescript-language-server",
+        "yaml-language-server",
+        "gopls",
+        "editorconfig-checker",
+        "prisma-language-server",
+        "svelte-language-server",
+        "sonarlint-language-server",
       },
-    }
-    -- Ensure the servers above are installed
-    local mason_lspconfig = require "mason-lspconfig"
-
-    mason_lspconfig.setup {
-      ensure_installed = vim.tbl_keys(servers),
-    }
-
-    mason_lspconfig.setup_handlers {
-      function(server_name)
-        require("lspconfig")[server_name].setup {
-          capabilities = capabilities,
-          on_attach = on_attach,
-          settings = servers[server_name],
-          filetypes = (servers[server_name] or {}).filetypes,
-        }
-      end,
-    }
-  end,
+    },
+    ---@param opts MasonSettings | {ensure_installed: string[]}
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require "mason-registry"
+      local function ensure_installed()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end
+      if mr.refresh then
+        mr.refresh(ensure_installed)
+      else
+        ensure_installed()
+      end
+    end,
+  },
 }
