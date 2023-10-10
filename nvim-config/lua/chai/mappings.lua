392702088eb0a6f8195629abfa633fa01741857e
vinchhay <vinchhay.sea@sisgroup.sg>
2023-10-10 11:37:54 +0700

    Better importing understanding

--- nvim-config/lua/chai/mappings.lua
+++ nvim-config/lua/chai/mappings.lua
@@ -1,59 +1,48 @@
 local M = {}
-local Utils = require "chai.utils"
 
 M.flash = {
-  {
-    "s",
-    mode = { "n", "x", "o" },
-    function()
-      require("flash").jump()
-    end,
-    desc = "[S]earch",
-  },
-  {
-    "S",
-    mode = { "o", "x" },
-    function()
-      require("flash").treesitter_search()
-    end,
-    desc = "[S]earch Treesitter",
-  },
-  {
-    "H",
-    mode = { "n", "o", "x" },
-    function()
-      require("flash").treesitter()
-    end,
-    desc = "Treesitter [H]ighlight",
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "[S]earch",
+    },
+    {
+      "S",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "[S]earch Treesitter",
+    },
+    {
+      "H",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Treesitter [H]ighlight",
+    },
   },
 }
 
 M.telescope = {
-  key = {
-    { mode = { "n" }, "<leader>,", "<cmd>Telescope buffers<cr>", desc = "List Buffers" },
-    { mode = { "n" }, "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Commands History" },
+  keys = {
+    { mode = { "n" }, "<leader>f?", require("telescope.builtin").oldfiles, desc = "[F]ind [?]recent Open Files " },
+    { mode = { "n" }, "<leader>,", require("telescope.builtin").buffers, desc = "List Buffers" },
+    { mode = { "n" }, "<leader>:", require("telescope.builtin").command_history, desc = "Commands History" },
 
     -- find
-    {
-      mode = { "n", "v" },
-      "<leader>fg",
-      Utils.telescope("grep_string", { word_match = "-w" }),
-      desc = "[F]ind [G]rep (root)",
-    },
-    {
-      mode = { "n", "v" },
-      "<leader>fG",
-      Utils.telescope("grep_string", { cwd = false, word_match = "-w" }),
-      desc = "[F]ind [G]rep (cwd)",
-    },
-    { mode = { "n" }, "<leader>ff", Utils.telescope "files", desc = "[F]ind [F]iles (root)" },
-    { mode = { "n" }, "<leader>fF", Utils.telescope "files", desc = "[F]ind [F]iles (cwd)" },
-    { mode = { "n" }, "<leader>fw", Utils.telescope "live_grep", desc = "[F]ind [W]ords (root)" },
-    { mode = { "n" }, "<leader>fW", Utils.telescope "live_grep", desc = "[F]ind [W]ords (cwd)" },
+    { mode = { "n" }, "<leader>ff", require("telescope.builtin").find_files, desc = "[F]ind [F]iles" },
+    { mode = { "n" }, "<leader>fg", require("telescope.builtin").grep_string, desc = "[F]ind [G]rep" },
+    { mode = { "n" }, "<leader>fw", require("telescope.builtin").live_grep, desc = "[F]ind [W]ords" },
     {
       mode = { "n" },
       "<leader>fs",
-      Utils.telescope("lsp_document_symbols", {
+      require("telescope.builtin").lsp_document_symbols {
         symbols = {
           "Class",
           "Function",
@@ -66,83 +55,29 @@ M.telescope = {
           "Field",
           "Property",
         },
-      }),
+      },
       desc = "[F]ind [S]ymbol",
     },
 
     -- git related
-    { mode = { "n" }, "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "[G]it [S]tatus" },
-  },
-  opt = {
-    i = {
-      ["<c-t>"] = function(...)
-        return require("trouble.providers.telescope").open_with_trouble(...)
-      end,
-      ["<a-t>"] = function(...)
-        return require("trouble.providers.telescope").open_selected_with_trouble(...)
-      end,
-      ["<a-i>"] = function()
-        local action_state = require "telescope.actions.state"
-        local line = action_state.get_current_line()
-        Utils.telescope("find_files", { no_ignore = true, default_text = line })()
-      end,
-      ["<a-h>"] = function()
-        local action_state = require "telescope.actions.state"
-        local line = action_state.get_current_line()
-        Utils.telescope("find_files", { hidden = true, default_text = line })()
-      end,
-      ["<C-Down>"] = function(...)
-        return require("telescope.actions").cycle_history_next(...)
-      end,
-      ["<C-Up>"] = function(...)
-        return require("telescope.actions").cycle_history_prev(...)
-      end,
-      ["<C-f>"] = function(...)
-        return require("telescope.actions").preview_scrolling_down(...)
-      end,
-      ["<C-b>"] = function(...)
-        return require("telescope.actions").preview_scrolling_up(...)
-      end,
-    },
-    n = {
-      ["q"] = function(...)
-        return require("telescope.actions").close(...)
-      end,
-    },
+    { mode = { "n" }, "<leader>gs", require("telescope.builtin").git_status, desc = "[G]it [S]tatus" },
   },
 }
 
 M.neotree = {
-  { mode = { "n" }, "<C-e>", "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
-  {
-    mode = { "n" },
-    "<leader>e",
-    function()
-      if vim.bo.filetype == "neo-tree" then
-        vim.cmd.wincmd "p"
-      else
-        vim.cmd.Neotree "focus"
-      end
-    end,
-    desc = "Toggle Explorer Focus",
-  },
-}
-
-M.mini = {
-  bufremove = {
+  keys = {
+    { mode = { "n" }, "<C-e>", "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
     {
-      "<leader>br",
+      mode = { "n" },
+      "<leader>e",
       function()
-        require("mini.bufremove").delete(0, false)
+        if vim.bo.filetype == "neo-tree" then
+          vim.cmd.wincmd "p"
+        else
+          vim.cmd.Neotree "focus"
+        end
       end,
-      desc = "[B]uffer [R]emove",
-    },
-    {
-      "<leader>bR",
-      function()
-        require("mini.bufremove").delete(0, true)
-      end,
-      desc = "[B]uffer [R]emove (Forced)",
+      desc = "Toggle Explorer Focus",
     },
   },
 }
