0da88dbb19f56d8256a77cbd3a84e1a8b6a2535f
vinchhay <vinchhay.sea@sisgroup.sg>
2023-10-11 11:29:39 +0700

    Refactoring and continues testing (11/*)

--- nvim-config/lua/chai/utils/init.lua
+++ nvim-config/lua/chai/utils/init.lua
@@ -63,4 +63,117 @@ function M.telescope(builtin, opts)
   end
 end
 
+-- [[ Configure LSP ]]
+--  This function gets run when an LSP connects to a particular buffer.
+M.on_attach = function(_, bufnr)
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = "LSP: " .. desc
+    end
+
+    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap("<leader>crn", vim.lsp.buf.rename, "[C]ode [R]e[n]ame")
+  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+  nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+  nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+  nmap("<leader>cd", vim.lsp.buf.type_definition, "[C]ode Type [D]efinition")
+
+  -- See `:help K` for why this keymap
+  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+  -- Lesser used LSP functionality
+  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+    vim.lsp.buf.format()
+  end, { desc = "Format current buffer with LSP" })
+end
+
+--  If you want to override the default filetypes that your language server will attach to you can
+--  define the property 'filetypes' to the map in question.
+M.servers = {
+  -- gopls = {},
+  -- rust_analyzer = {},
+  tsserver = {
+    keys = {
+      "<leader>coi",
+      function()
+        vim.lsp.buf.code_action { apply = true, context = { only = { "source.organizeImports.ts" }, diagnostics = {} } }
+      end,
+      desc = "[C]ode [O]rganize [I]mport",
+    },
+    settings = {
+      completions = {
+        completeFunctionCalls = true,
+      },
+    },
+  },
+  eslint = {
+    settings = {
+      workingDirectory = { mode = "auto" },
+    },
+  },
+  jsonls = {
+    -- lazy-load schemastore when needed
+    on_new_config = function(new_config)
+      new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+      vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+    end,
+    settings = {
+      json = {
+        format = {
+          enable = true,
+        },
+        validate = { enable = true },
+      },
+    },
+  },
+  yamlls = {
+    -- Have to add this for yamlls to understand that we support line folding
+    capabilities = {
+      textDocument = {
+        foldingRange = {
+          dynamicRegistration = false,
+          lineFoldingOnly = true,
+        },
+      },
+    },
+    -- lazy-load schemastore when needed
+    on_new_config = function(new_config)
+      new_config.settings.yaml.schemas = vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
+    end,
+    settings = {
+      redhat = { telemetry = { enabled = false } },
+      yaml = {
+        keyOrdering = false,
+        format = {
+          enable = true,
+        },
+        validate = true,
+        schemaStore = {
+          -- Must disable built-in schemaStore support to use
+          -- schemas from SchemaStore.nvim plugin
+          enable = false,
+          -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+          url = "",
+        },
+      },
+    },
+  },
+  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+
+  lua_ls = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+    },
+  },
+}
+
 return M
