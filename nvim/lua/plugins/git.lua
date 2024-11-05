8d12fba182e876683b084fe1bbe3999b1cfc3bc3
chh-ay <vinchhaysea30@gmail.com>
2024-11-05 12:16:19 +0700

    Adjusted telescope defaults and git config blame line working correctly

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,9 +1,6 @@
 return {
   "lewis6991/gitsigns.nvim",
-  opts = function(_, opts)
-    vim.list_extend(opts, {
-      signs_staged_enable = true,
-      current_line_blame = true,
-    })
-  end,
+  opts = {
+    current_line_blame = true,
+  },
 }
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      prompt_prefix = "   ",
+      selection_caret = "  ",
+      layout_config = {
+        horizontal = {
+          width = 0.85,
+          height = 0.85,
+          preview_width = 0.55,
+        },
+      },
+    },
+  },
+}
